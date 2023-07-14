ff9c8659198501706a9ce54f113b4bfd0571d736
giancarlo <giancarlo@gemlabs.dev>
2023-07-14 10:06:12 +0200

    we can now clear the cache

--- bot/trade/routes.py
+++ bot/trade/routes.py
@@ -2,6 +2,8 @@ import json
 
 from bingX.perpetual.v1 import Perpetual
 from flask import request, g
+
+from ..cache import Cache
 from ..services.order_service import OrderService
 from ..trade import bp
 from ..db import store_user_credentials
@@ -14,6 +16,7 @@ def find_user_credentials():
 
 @bp.route('/trade/<exchange>', methods=['POST'])
 def perpetual_order(exchange):
+    print(Cache.get_exchange(exchange))
     credentials = get_user_credentials(exchange)
     if credentials is None:
         return json.dumps({'status': 'NO_CREDENTIALS_FOUND'}), 400
@@ -26,9 +29,17 @@ def perpetual_order(exchange):
                            action=trade['action'],
                            quantity=trade['quantity'],
                            trade_type=trade['trade_type'],
-                           leverage=trade['leverage'] if 'leverage' in trade else 1)
+                           leverage=trade['leverage'] if 'leverage' in trade else 1,
+                           safety=trade['safety'] if 'safety' in trade else False
+                           )
 
     return service.start_order()
+
+
+@bp.route('/trade/clear-cache', methods=['POST'])
+def clear_cache():
+    Cache.clear_cache()
+    return json.dumps({'status': 'SUCCESS', 'msg': 'cache cleared'}), 200
 
 
 def get_user_credentials(exchange):
