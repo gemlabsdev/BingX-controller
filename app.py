ffb295abf44e120b168fd5fbeda0794eb9978242
giancarlo <giancarlo@gemlabs.dev>
2023-07-14 22:10:41 +0200

    Updates requirements adds import select atop of the app.py file fixes issue with monkeyPatch by moving it right under it import statement

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-import os
+import select
 import eventlet
+
+eventlet.monkey_patch(all=True)
+
 from eventlet import wsgi
+import os
 import bot
 
 app = bot.create_app()
 
 if __name__ == '__main__':
-    # eventlet.monkey_patch(all=True)
     port = int(os.environ.get('PORT', 3000))
     server = eventlet.listen(('0.0.0.0', port))
     wsgi.server(server, app)
--- bot/config.py
+++ bot/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv()
 
+
 class Config:
     MONGO_URI = f"mongodb+srv://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}.mongodb.net/{os.environ['DB_NAME']}?retryWrites=true&w=majority"
 
